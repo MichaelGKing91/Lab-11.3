85a82027698a335cc73c21dc092609c2db494458
MichaelGKing91 <michaelking@quickenloans.com>
2021-05-12 17:51:41 -0400

    forst

---
+++ Lab 11.3/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Lab_11._3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab_11._3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            // Add Validation here
+            if (user.password != user.passcheck)
+            {
+                return Content("Your passwords did not match!" +
+                    " Your profile has not been created.");
+            }
+            string emailVal = @"^\w{1,36}@\w{1,36}\.\w{2,36}$";
+            Regex myRegex = new Regex(emailVal);
+            if (!myRegex.IsMatch(user.email))
+            {
+                return Content("Your email was invalid!" +
+                    " Your profile has not been created.");
+            }
+            string phoneVal = @"^[0-9]{1,2}\([0-9]{3}\)[0-9]{3}-[0-9]{4}$";
+            myRegex = new Regex(phoneVal);
+            if (!myRegex.IsMatch(user.phone))
+            {
+                return Content("Your phone number was invalid!" +
+                    " \nYour profile has not been created. \nPlease use the format: #(###)###-#### ");
+            }
+            return View(user);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab 11.3/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_11._3.Models
+{
+    public class User
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string favcoffee { get; set; }
+        public string favpastry { get; set; }
+        public string password { get; set; }
+        public string passcheck { get; set; }
+
+    }
+}
